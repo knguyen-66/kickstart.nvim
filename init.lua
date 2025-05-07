72ace88a249124fb987d5bafb6c27795e9a6132c
Khanh Nguyen <khanhnguyen02311@gmail.com>
2025-05-07 14:33:38 +0700

    feat: add goimports to formatter

--- init.lua
+++ init.lua
@@ -777,6 +777,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'goimports', 'gofmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
